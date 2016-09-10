a063904546f79050ad8d3bcd1ed52e12d9bbfbbe
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2016-09-10 08:13:25 +0700

    Pilih gender yang sesuai karena harus di akses

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1033/pendaftaranperguruantinggi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1033/pendaftaranperguruantinggi/MainActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -12,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     EditText etnama;
     Button bOK;
     TextView tvhasil;
+    RadioGroup rgstatus;
+    String hasil;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,17 +23,34 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etnama = (EditText) findViewById(R.id.editTextnama);
+//        rb1 = (RadioButton) findViewById(R.id.radioButton);
+//        rb2 = (RadioButton) findViewById(R.id.radioButton2);
+        rgstatus = (RadioGroup) findViewById(R.id.radiogroup);
         bOK = (Button) findViewById(R.id.button);
         tvhasil = (TextView) findViewById(R.id.textView6);
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String nama = etnama.getText().toString();
-                tvhasil.setText("Nama : " + nama);
+                doProcess();
             }
         });
     }
 
+    private void doProcess() {
+        if (isValid()) {
+            String nama = etnama.getText().toString();
+            //tvhasil.setText("Nama : " + nama);
+            if (rgstatus.getCheckedRadioButtonId() != -1) {
+                RadioButton rb = (RadioButton) findViewById(rgstatus.getCheckedRadioButtonId());
+                hasil = rb.getText().toString();
+            }
+            if (hasil == null) {
+                tvhasil.setText("Belum memilih jenis kelamin");
+            } else {
+                tvhasil.setText("Nama : " + nama + "\n" + "Jenis Kelamin : " + hasil);
+            }
+        }
+    }
 
     private boolean isValid() {
         boolean valid = true;
@@ -41,9 +62,10 @@ public class MainActivity extends AppCompatActivity {
         } else if (nama.length() < 3) {
             etnama.setError("Nama Minimal 3 Karakter");
             valid = false;
-        } else {
-            etnama.setError(null);
         }
+
+        //String hasil = null;
+
         return valid;
     }
 }
