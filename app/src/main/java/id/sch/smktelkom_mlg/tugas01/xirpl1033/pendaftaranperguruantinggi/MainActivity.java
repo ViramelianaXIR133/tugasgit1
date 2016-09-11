e7b3ecbddfe9ee5ebdcd5b0ed37fd0617d514579
Vira Meliana Agustin <viramelianaagustin@gmail.com>
2016-09-11 12:50:53 +0700

    Klik DAFTAR agar pendaftaran anda dapat di proses

--- app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1033/pendaftaranperguruantinggi/MainActivity.java
+++ app/src/main/java/id/sch/smktelkom_mlg/tugas01/xirpl1033/pendaftaranperguruantinggi/MainActivity.java
@@ -12,16 +12,19 @@ import android.widget.RadioGroup;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+
 public class MainActivity extends AppCompatActivity implements CompoundButton.OnCheckedChangeListener {
 
     EditText etnama;
     Button bOK;
-    TextView tvhasil;
     TextView tvjurusan;
-    Spinner spasal;
-    TextView tvasal;
+    TextView tvnama;
+    TextView tvradio;
+    TextView spiner;
+    TextView tvhasil2;
+    Spinner spkota;
+    int njurusan;
     RadioGroup rgstatus;
-    String hasil;
     CheckBox cb1, cb2, cb3;
 
     @Override
@@ -29,9 +32,6 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        //etnama = (EditText) findViewById(R.id.editTextnama);
-//        rb1 = (RadioButton) findViewById(R.id.radioButton);
-//        rb2 = (RadioButton) findViewById(R.id.radioButton2);
         rgstatus = (RadioGroup) findViewById(R.id.radiogroup);
         bOK = (Button) findViewById(R.id.button);
         cb1 = (CheckBox) findViewById(R.id.checkBox);
@@ -41,69 +41,67 @@ public class MainActivity extends AppCompatActivity implements CompoundButton.On
         cb1.setOnCheckedChangeListener(this);
         cb2.setOnCheckedChangeListener(this);
         cb3.setOnCheckedChangeListener(this);
-        tvjurusan = (TextView) findViewById(R.id.textView6);
-        tvhasil = (TextView) findViewById(R.id.textView6);
-        spasal = (Spinner) findViewById(R.id.spinnerkota);
-        tvasal = (TextView) findViewById(R.id.textView6);
+        etnama = (EditText) findViewById(R.id.editTextnama);
+        tvnama = (TextView) findViewById(R.id.textView6);
+        tvjurusan = (TextView) findViewById(R.id.textView9);
+        tvradio = (TextView) findViewById(R.id.textView10);
+        spiner = (TextView) findViewById(R.id.textView11);
+        tvhasil2 = (TextView) findViewById(R.id.textView8);
+        spkota = (Spinner) findViewById(R.id.spinnerkota);
         bOK.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                doProcess();
+                doMulai();
+                doClick();
+                doProces();
+                doSiap();
             }
         });
     }
 
-    private void doProcess() {
-        if (isValid()) {
-            String nama = etnama.getText().toString();
-            //tvhasil.setText("Nama : " + nama);
-            if (rgstatus.getCheckedRadioButtonId() != -1) {
-                RadioButton rb = (RadioButton) findViewById(rgstatus.getCheckedRadioButtonId());
-                hasil = rb.getText().toString();
-            }
-            if (hasil == null) {
-                tvhasil.setText("Belum memilih jenis kelamin");
-            } else {
-                tvhasil.setText("Nama : " + nama + "\n" + "Jenis Kelamin : " + hasil);
-            }
-        }
-        String jurusan = "Jurusan : ";
-        String nama = etnama.getText().toString();
+    private void doMulai() {
+        //String nama = etnama.getText().toString();
 
+        if (etnama.getText().toString().isEmpty()) {
+            etnama.setError("Nama Belum Di Isi");
+        } else if (etnama.getText().toString().length() < 3) {
+            etnama.setError("Nama Minimal 3 Karakter");
+        } else {
+            etnama.setError(null);
+        }
+        tvnama.setText("Nama : " + etnama.getText().toString());
+    }
+
+    private void doSiap() {
+        String hasil = null;
+        if (rgstatus.getCheckedRadioButtonId() != -1) {
+            RadioButton rb = (RadioButton) findViewById(rgstatus.getCheckedRadioButtonId());
+            hasil = rb.getText().toString();
+        }
+        tvradio.setText("Jenis Kelamin : " + hasil);
+    }
+
+    private void doProces() {
+        String jurusan = "Jurusan : ";
         int startien = jurusan.length();
         if (cb1.isChecked()) jurusan += cb1.getText() + "\n";
         if (cb2.isChecked()) jurusan += cb2.getText() + "\n";
         if (cb3.isChecked()) jurusan += cb3.getText() + "\n";
         if (jurusan.length() == startien) jurusan += "Tidak ada pilihan";
-
-        //  int asal = spasal.getSelectedItemPosition();
-//
-//        String[] arProvinsi = getResources().getStringArray(R.array.kota);
-//        int asal = spasal.getSelectedItemPosition();
-
-        String asal = spasal.getSelectedItem().toString();
-        tvjurusan.setText("Nama : " + nama + "\n" + "Jenis Kelamin : " + "\n" + hasil + jurusan + "Asal Kota : " + asal);
-
+        tvjurusan.setText(jurusan);
     }
 
-    //
-    private boolean isValid() {
-        boolean valid = true;
-        String nama = etnama.getText().toString();
-
-        if (nama.isEmpty()) {
-            etnama.setError("Nama Belum Di Isi");
-            valid = false;
-        } else if (nama.length() < 3) {
-            etnama.setError("Nama Minimal 3 Karakter");
-            valid = false;
-        }
-
-        return valid;
+    private void doClick() {
+        String asal = spkota.getSelectedItem().toString();
+        spiner.setText("Asal : " + asal);
+//        tvjurusan.setText("Nama : " + nama + "\n" + "Jenis Kelamin : " + "\n" + hasil + jurusan + "\n" + "Asal" +
+//                asal + "Identitas anda akan di kirim ke kota asal anda agar segera di akses.");
     }
 
     @Override
     public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+        if (isChecked) njurusan += 1;
+        else njurusan -= 1;
+        tvhasil2.setText("Jurusan ( " + njurusan + "terpilih)");
     }
 }
